d99df928222ce8a9d5d28a9378fe4bbd5f6572b4
Carlos Roberto Arias Arevalo <arias@spu.edu>
2022-05-17 10:44:22 -0700

    More phone book operations

--- phone_book.cpp
+++ phone_book.cpp
@@ -4,6 +4,8 @@
 
 #include "phone_book.h"
 
+#include <cstring>
+
 PhoneBook::PhoneBook() {
     _size = 0;
     for (int i = 0; i < MAX_FRIENDS; ++i) {
@@ -19,22 +21,47 @@ PhoneBook::PhoneBook(const PhoneBook &pb) {
 }
 
 PhoneBook::~PhoneBook() {
+    for (int i = 0; i < _size; ++i) {
+        delete _data[i];
+    }
 }
 
 const PhoneBook &PhoneBook::operator=(const PhoneBook &rhs) {
+    if (this == &rhs)
+        return *this;
+    for (int i = 0; i < _size; ++i) {
+        delete _data[i];
+    }
+    _size = rhs._size;
+    for (int i = 0; i < _size; ++i) {
+        _data[i] = new Person(*rhs._data[i]);
+    }
     return *this;
 }
 
 bool PhoneBook::AddPerson(const Person &person) {
-    return false;
+    if (_size == MAX_FRIENDS)
+        return false;
+    _data[_size] = new Person(person);
+    _size++;
+    return true;
 }
 
 bool PhoneBook::RemovePerson(const char *name) {
-    return false;
+    // Find the name position
+    // if not found, return false
+    // if found, delete the person in that position
+    // shift the elements after that position to the "left"
+    // set the "last" element to nullptr
+    // return true
 }
 
 Person PhoneBook::FindPerson(const char *name) const {
-    return Person(nullptr, nullptr);
+    for (int i = 0; i < _size; ++i) {
+        if (strcmp(name, _data[i]->GetName().c_str()) == 0)
+            return *_data[i];
+    }
+    return Person("","",0);
 }
 
 string PhoneBook::ToJSON() const {
